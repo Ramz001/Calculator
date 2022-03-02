2e0103a24e78e168c5c372ae5db44959493097a3
Ramz001 <rkenjaev1@gmail.com>
2022-03-02 16:44:35 +0500

    main

--- src/script.js
+++ src/script.js
@@ -1,52 +1,74 @@
-const numbers = document.querySelectorAll("[data-number]")
-const display = document.querySelector("#display")
-const deleteBtn = document.querySelector("[data-delete]")
-const resetBtn = document.querySelector('[data-reset]')
-const operationBtns = document.querySelectorAll("[data-operation]")
-const decimalBtn = document.querySelector("[data-decimal]")
-const equalBtn = document.querySelector("[data-equal]")
+const numbers = document.querySelectorAll("[data-number]");
+const display = document.querySelector("#display");
+const deleteBtn = document.querySelector("[data-delete]");
+const resetBtn = document.querySelector("[data-reset]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const decimalBtn = document.querySelector("[data-decimal]");
+const equalBtn = document.querySelector("[data-equal]");
+const rangeBtn = document.querySelector("[data-range]");
 
-let buffer = ''
+let buffer = "";
 
 // Event listeners
-numbers.forEach(num=>{
-    num.addEventListener("click",(e)=>{
-        const currentNumber = e.currentTarget.dataset.number
-        buffer += currentNumber
-        displayElement(buffer)
-    })
-})
+numbers.forEach((num) => {
+  num.addEventListener("click", (e) => {
+    const currentNumber = e.currentTarget.dataset.number;
+    buffer += currentNumber;
 
-resetBtn.addEventListener("click",()=>{
-    display.value = ""
-    buffer = ''
-})
+    displayElement(buffer);
+  });
+});
 
-operationBtns.forEach(op =>{
-    op.addEventListener('click',(e)=>{
-        const currentOperation = e.currentTarget.dataset.operation
-        buffer += currentOperation
-        displayElement(buffer)
-    })
-})
+resetBtn.addEventListener("click", () => {
+  display.value = "";
+  buffer = "";
+});
 
-decimalBtn.addEventListener("click",(e)=>{
-    const decimal = e.currentTarget.dataset.decimal
-    buffer += decimal
-    displayElement(buffer)
-})
+operationBtns.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    const currentOperation = e.currentTarget.dataset.operation;
+    buffer += currentOperation;
+    displayElement(buffer);
+  });
+});
 
-equalBtn.addEventListener("click",()=>{
-    buffer = eval(buffer)
-    displayElement(buffer)
-})
+decimalBtn.addEventListener("click", (e) => {
+  const decimal = e.currentTarget.dataset.decimal;
+  buffer += decimal;
+  displayElement(buffer);
+});
 
-deleteBtn.addEventListener("click",()=>{
-    buffer = buffer.toString().slice(0,-1)
-    displayElement(buffer)
-})
+equalBtn.addEventListener("click", () => {
+  buffer = eval(buffer);
+  if (buffer === undefined) return "";
+  displayElement(buffer);
+});
 
-function displayElement(element){
-    display.value = element
+deleteBtn.addEventListener("click", () => {
+  buffer = buffer.toString().slice(0, -1);
+  displayElement(buffer);
+});
+
+function displayElement(element) {
+  display.value = element;
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  rangeBtn.value = 1;
+});
+
+rangeBtn.addEventListener("change", () => {
+  if (rangeBtn.value == 1) {
+    document.body.classList.remove("theme-2");
+    document.body.classList.remove("theme-3");
+  }
+
+  if (rangeBtn.value == 2) {
+    document.body.classList.add("theme-2");
+    document.body.classList.remove("theme-3");
+  }
+  if (rangeBtn.value == 3) {
+    document.body.classList.add("theme-3");
+    document.body.classList.remove("theme-2");
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,44 +8,31 @@ module.exports = {
       fontFamily:{
         'spartan' : ["Spartan"]
       },
-      colors:{
-        theme1:{
-          "very-dark-desaturated-blue": 'hsl(222, 26%, 31%);',
-          "very-dark-desaturated-blue-toggle": "hsl(223, 31%, 20%)",
-          "very-dark-desaturated-blue-screen": "hsl(224, 36%, 15%)",
-          "desaturated-dark-blue": "hsl(225, 21%, 49%)",
-          "desaturated-dark-blue-shadow": "hsl(224, 28%, 35%)",
-          "red": "hsl(6, 63%, 50%)",
-          "dark-red": "hsl(6, 70%, 34%)",
-          "light-grayish-orange": "hsl(30, 25%, 89%)",
-          "grayish-orange": "hsl(28, 16%, 65%)",
-          "very-dark-grayish-blue": "hsl(221, 14%, 31%)",
+      textColor: {
+        skin: {
+          'key-base': 'var(--color-text-key-base)',
+          'key-accent-1': 'var(--color-text-key-accent-1)',
+          'key-accent-2': 'var(--color-text-key-accent-2)',
+          screen: 'var(--color-text-screen)',
         },
-        theme2:{
-          "light-gray": "hsl(0, 0%, 90%)",
-          "grayish-red": "hsl(0, 5%, 81%)",
-          "very-light-gray": "hsl(0, 0%, 93%)",
-          "dark-moderate-cyan": "hsl(185, 42%, 37%)",
-          "very-dark-cyan": "hsl(185, 58%, 25%)",
-          "orange": "hsl(25, 98%, 40%)",
-          "dark-orange": "hsl(25, 99%, 27%)",
-          "light-grayish-yellow": "hsl(45, 7%, 89%)",
-          "dark-grayish-orange": "hsl(35, 11%, 61%)",
-          "very-dark-grayish-yellow": "hsl(60, 10%, 19%)",
+      },
+
+      backgroundColor: {
+        skin: {
+          base: 'var(--color-fill-base)',
+          screen: 'var(--color-fill-screen)',
+          keypad: 'var(--color-fill-keypad)',
+          'key-base': 'var(--color-fill-key-base)',
+          'key-accent-1': 'var(--color-fill-key-accent-1)',
+          'key-accent-2': 'var(--color-fill-key-accent-2)',
         },
-        theme3:{
-          "very-dark-violet": 'hsl(268, 75%, 9%)',
-          "very-dark-violet-toggle": "hsl(268, 71%, 12%)",
-          "dark-violet": 'hsl(281, 89%, 26%)',
-          "vivid-magenta": "hsl(285, 91%, 52%)",
-          "pure-cyan": "hsl(176, 100%, 44%)",
-          "soft-cyan": "hsl(177, 92%, 70%)",
-          'very-dark-violet': "hsl(268, 47%, 21%)",
-          "dark-magenta": "hsl(290, 70%, 36%)",
-          "light-yellow": "hsl(52, 100%, 62%)",
-          "very-dark-blue": "hsl(198, 20%, 13%)",
-        }
-      }
+      },
+
+      boxShadow: {
+        'key-base': '0px 4px var(--color-shadow-key-base)',
+        'key-accent-1': '0px 4px var(--color-shadow-key-accent-1)',
+        'key-accent-2': '0px 4px var(--color-shadow-key-accent-2)',
+      },
       
     },
   },
